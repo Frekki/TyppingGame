e8c048da75b97035e4bbada25675400b8972159b
Frekky <sobolrip1@gmail.com>
2018-02-08 14:57:26 +0200

    Initial Commit

---
+++ FirstFormGame/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstFormGame
+{
+    public partial class Form1 : Form
+    {
+
+        Random random = new Random();
+        Stats stats = new Stats();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if(listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Лошара!");
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //If correct key selected, delet letter from ListBox, and leteers uppear speed up
+            if(listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                if (timer1.Interval > 400)
+                    timer1.Interval -= 10;
+                if (timer1.Interval > 250)
+                    timer1.Interval -= 7;
+                if (timer1.Interval > 100)
+                    timer1.Interval -= 2;
+                difficultyProgressBar.Value = 800 - timer1.Interval;
+
+                //When the correct button pushed, refresh Stats object, call Update() method with true argument
+                stats.Update(true);
+            }
+            else
+            {
+                //When incorrect button pushed update Stats object, and call Update() method with false argument
+                stats.Update(false);
+            }
+
+            //Update pointers of StatusStrip element
+            correctLabel.Text = "Correct: " + stats.Correct;
+            missedLabel.Text = "Missed: " + stats.Missed;
+            totalLabel.Text = "Total: " + stats.Total;
+            accuracyLabel.Text = "Accuracy: " + stats.Accuracy;
+        }
+    }
+}
